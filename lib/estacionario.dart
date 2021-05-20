6a41216f8932c8b65c61068eb085fac04307afd1
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-05-20 11:50:53 -0300

    Não saí do canto

---
+++ lib/estacionario.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Estacionario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Segunda Tela"),
+    );
+  }
+}
