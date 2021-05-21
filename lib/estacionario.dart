4d84e4f6ee53ba1e7e06b63960d178d3c5c2b88a
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-05-21 11:42:48 -0300

    SplashScreen
    Graças a Deus, dei o primeiro passo!

--- lib/estacionario.dart
+++ lib/estacionario.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class Estacionario extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Segunda Tela"),
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Text('O meu sonho começa aqui'),
+      ),
     );
   }
 }
