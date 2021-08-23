509ff7f2ea4b25d6f8be40433b04bc818a12a6e8
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-08-23 12:17:41 -0300

    Primeira parte do estacionario
    Agora vai!

--- lib/paginas/estacionario.dart
+++ lib/paginas/estacionario.dart
@@ -10,8 +10,52 @@ class Estacionario extends StatefulWidget {
 class _EstacionarioState extends State<Estacionario> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('A bosta fede aqui - Segunda Tela'),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/menu.jpg'),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                )),
+            child: Column(
+              children: [
+                Row(),
+                SizedBox(height: 20),
+                Image(
+                  // height: 80,
+                  // width: 30,
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Guarda Corpo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Segoe Black',
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  'Um app sobre saúde e segurança do trabalho',
+                  style:
+                      TextStyle(color: Colors.white, fontFamily: 'Segoe Light'),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          )
+
+          ///AQUI
+        ],
+      ),
     );
   }
 }
