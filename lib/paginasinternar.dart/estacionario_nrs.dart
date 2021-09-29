26c0e439caf4b46e614d6f9e1b8c901c2ad5afb2
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-09-29 10:07:16 -0300

    Atualização da nova tela secundária das nrs

--- lib/paginasinternar.dart/estacionario_nrs.dart
+++ lib/paginasinternar.dart/estacionario_nrs.dart
@@ -22,8 +22,9 @@ class NormasEstacionarios extends StatelessWidget {
               padding: EdgeInsets.only(top: 50, left: 20, right: 30),
               // color: Colors.white,
               width: MediaQuery.of(context).size.width,
-              height: 300,
+              height: MediaQuery.of(context).size.height * 0.3,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
@@ -31,12 +32,115 @@ class NormasEstacionarios extends StatelessWidget {
                         Icons.arrow_back_ios_rounded,
                         size: 20,
                         color: Colors.white,
+                      ),
+                      Expanded(child: Container()),
+                      Icon(
+                        Icons.media_bluetooth_on,
+                        size: 20,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    'Oremos e vamos por aqui',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Segunda linha de texto',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomLeft,
+                              colors: [
+                                Colors.white10,
+                                Colors.white30,
+                              ]),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              size: 20,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              '68 min',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 30),
+                      Container(
+                        // width: MediaQuery.of(context).size.width * 0.5,
+                        width: 200,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomLeft,
+                              colors: [
+                                Colors.white10,
+                                Colors.white30,
+                              ]),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.handyman_outlined,
+                              size: 20,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'Alguns texto ale. aqui',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
                       )
                     ],
                   )
                 ],
               ),
-            )
+            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topRight: Radius.circular(70)),
+              ),
+            ))
           ],
         ),
       ),
