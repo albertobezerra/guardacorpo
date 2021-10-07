444becd9298aa5b5f369bb595c92af39c4536147
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-10-07 13:49:09 -0300

    NR1 com leitura de pdf

--- lib/normas/nr1.dart
+++ lib/normas/nr1.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class NR1 extends StatelessWidget {
+class NR1 extends StatefulWidget {
   const NR1({Key? key}) : super(key: key);
 
   @override
+  _NR1State createState() => _NR1State();
+}
+
+class _NR1State extends State<NR1> {
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: SfPdfViewer.asset(
+        'assets/nr.pdf',
+        key: _pdfViewerKey,
+      ),
+    );
   }
 }
