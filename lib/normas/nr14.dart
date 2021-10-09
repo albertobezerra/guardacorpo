ca28f359d85ce677a0cc208bf195bf664bd6f557
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-10-09 10:41:51 -0300

    Finalizando das telas das nrs até a 15a1

--- lib/normas/nr14.dart
+++ lib/normas/nr14.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:guardacorpo/paginas/nrs_estacionario.dart';
+import 'package:native_admob_flutter/native_admob_flutter.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class NR14 extends StatelessWidget {
   const NR14({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff0C5422),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(80),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 60, left: 30),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 26,
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.zero,
+                          constraints: BoxConstraints(),
+                          onPressed: () {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => NrsEstacionario()));
+                          },
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            flex: 2,
+          ),
+          Flexible(
+            child: SfPdfViewer.asset(
+              'assets/pdf_nr/nr14.pdf',
+              key: _pdfViewerKey,
+            ),
+            flex: 12,
+          ),
+          Flexible(
+            child: Container(
+              color: Colors.redAccent,
+              child: BannerAd(size: BannerSize.ADAPTIVE),
+            ),
+            flex: 1,
+          ),
+        ],
+      ),
     );
   }
 }
