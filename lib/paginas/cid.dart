f858191df355229146f69335aaea3ff9f8654e0d
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-10-15 03:14:23 -0300

    Módulo de CID finalizado

--- lib/paginas/cid.dart
+++ lib/paginas/cid.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:native_admob_flutter/native_admob_flutter.dart';
 
 class Cid extends StatelessWidget {
   const Cid({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff0C5422),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(80),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 60, left: 30),
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 26,
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.zero,
+                          constraints: BoxConstraints(),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'CID'.toUpperCase(),
+                          style: TextStyle(
+                            fontFamily: 'Segoe Black',
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            flex: 2,
+          ),
+          Flexible(
+              flex: 12,
+              child: WebView(
+                initialUrl: 'https://cid10.com.br/%5Ecode',
+                javascriptMode: JavascriptMode.unrestricted,
+              )
+              // WebView(
+              //   initialUrl: 'flutter.dev',
+              //   javascriptMode: JavascriptMode.unrestricted,
+              // )
+
+              ),
+          Flexible(
+            child: Container(
+              child: BannerAd(size: BannerSize.ADAPTIVE),
+            ),
+            flex: 1,
+          ),
+        ],
+      ),
+    );
   }
 }
