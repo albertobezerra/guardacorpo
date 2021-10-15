43b66f4d3ef90f344f1b90e4d3038ca58a25e9a6
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2021-10-15 13:32:23 -0300

    Módulo de Consulta de CNPJ finalizado

--- lib/paginas/cnpj.dart
+++ lib/paginas/cnpj.dart
@@ -1,12 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:native_admob_flutter/native_admob_flutter.dart';
 
 class CNPJ extends StatelessWidget {
-  const CNPJ({ Key? key }) : super(key: key);
+  const CNPJ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff0C5422),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(80),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 60, left: 30),
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            size: 26,
+                            color: Colors.white,
+                          ),
+                          padding: EdgeInsets.zero,
+                          constraints: BoxConstraints(),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Consulta de CNPJ'.toUpperCase(),
+                          style: TextStyle(
+                            fontFamily: 'Segoe Black',
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            flex: 2,
+          ),
+          Flexible(
+              flex: 12,
+              child: Container(
+                child: WebView(
+                  initialUrl: 'https://www.situacaocadastral.com.br/',
+                  javascriptMode: JavascriptMode.unrestricted,
+                ),
+              )),
+          Flexible(
+            child: Container(
+              child: BannerAd(size: BannerSize.ADAPTIVE),
+            ),
+            flex: 1,
+          ),
+        ],
+      ),
     );
   }
 }
